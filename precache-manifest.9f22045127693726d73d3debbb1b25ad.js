e3660c3f51f634427a7826c04b62371b6e6b8be8
Belga-JS <Meherzi.belga@yahoo.com>
2019-12-16 10:11:54 +0100

    Updates

---
+++ precache-manifest.9f22045127693726d73d3debbb1b25ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5dc3336188315d048ef108d42dca5b",
+    "url": "/todo-redux/index.html"
+  },
+  {
+    "revision": "2e9b1546bb8fa5b85d1a",
+    "url": "/todo-redux/static/css/main.83a08a0a.chunk.css"
+  },
+  {
+    "revision": "6a90234f2d16b504b700",
+    "url": "/todo-redux/static/js/2.61db4bec.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/todo-redux/static/js/2.61db4bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e9b1546bb8fa5b85d1a",
+    "url": "/todo-redux/static/js/main.746f5781.chunk.js"
+  },
+  {
+    "revision": "a24a0e4a160e595c7926",
+    "url": "/todo-redux/static/js/runtime-main.37ddefc5.js"
+  }
+]);
